e385b175b7af7e07999abaad9dc879566276b3bd
RafaPaludo <rafael.ricardopp@gmail.com>
2024-02-25 12:16:16 -0300

    docs: add jsdocs to utils functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,20 @@
 const fs = require('node:fs')
 
+/**
+  * Read JSON file content synchronously.
+  * @param {String} path - Path to json file.
+  * @returns {Object} 
+*/
 function read (path) {
   return JSON.parse(fs.readFileSync(path, 'utf-8'))
 }
 
+/**
+  * Write content to JSON file synchronously.
+  * @param {String} path - Path to json file.
+  * @param {Object} user - User data.
+  * @param {String} usersFile - Name of file to be written.
+*/
 function write (path, user, usersFile) {
   usersFile.push(user)
 
